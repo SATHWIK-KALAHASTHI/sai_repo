0746ebfad766338a4c960e8f9740eb686525e0d6
SATHWIK-KALAHASTHI <kvnssathwik.03@gmail.com>
2024-09-11 15:44:11 +0530

    done

--- welcome.java
+++ welcome.java
@@ -1,6 +1,7 @@
 public class Welcome{
     public static void main(String[] args) {
         System.out.println("welcome to java");
+        System.out.println("Hello man");
         System.out.println(args[0]);
     }
 }
